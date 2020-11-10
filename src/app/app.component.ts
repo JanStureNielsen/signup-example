d6eedb922d367ba490cb001584b8112265227587
Jan Nielsen <jan.sture.nielsen@gmail.com>
2020-11-10 09:20:42 -0700

    Initial sample.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder
+  , FormGroup
+  , Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'signup-example';
+  title = "signup-example";
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+      this.form = this.fb.group({
+          firstName: ['', []],
+          lastName: ['', []],
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(6)]],
+          passwordConfirm: ['', Validators.required],
+      }, {
+          //validator: MustMatch('password', 'passwordConfirm')
+      });
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  get passwordConfirm() {
+    return this.form.get('passwordConfirm');
+  }
+
+  onSubmit() {
+    console.log('jsn: on-submit', this.form);
+    if (!this.form.invalid) {
+      console.log("SUBMIT: ", this.form);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexModule } from '@angular/flex-layout';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    FlexModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
+   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
